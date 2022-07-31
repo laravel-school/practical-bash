249f75ac64883fa13a883a876ec96bb3fc390f88
Thouhedul Islam <tisuchi@gmail.com>
2022-07-31 21:34:55 +0200

    add string and file manipulation

---
+++ fileManupulation.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Writing to a file
+echo "I am going to write this line to the foo.txt file." > foo.txt
+
+# Over write
+echo "The previous line will be overwrite with this line." > foo.txt
+
+# Appending to a file
+echo "This will be continue after the previous line." >> foo.txt
+
+
---
+++ strings.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Concatanatiion
+echo "===== Concatanation ====="
+echo "Foo " "Bar " "Baz"
+
+# Continuation
+echo "===== Contninuation ====="
+echo "Foo Bar \
+Baz"
+
+# Print with <Tab>
+echo "===== Prining with Tab ====="
+echo -e "Foo\tBar\tBaz"
+
+# Print with <New Line>
+echo "===== Prining with New Line ====="
+echo -e "Foo\nBar\nBaz"
